cd878113909099a5031093c8efff0445317a2878
BryanLarico <blaricor@unsa.edu.pe>
2024-06-12 12:40:49 -0500

    Templates con forms planos GET: creando un buscador search.html de la documentacion de DJango 5.0, agregando su views.py y subiendo a urls.py

--- listaContactos/urls.py
+++ listaContactos/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from inicio.views import myHomeview
 from inicio.views import anotherView
-from personas.views import personaTestView, personaCreateView
+from personas.views import personaTestView, personaCreateView, searchForHelp
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,4 +26,5 @@ urlpatterns = [
     path('', myHomeview, name="Pagina de inicio"),
     path('personas/', personaTestView, name='otro'),
     path('agregar/', personaCreateView , name='createPersona'),
+    path('search/', searchForHelp, name='buscar')
 ]
